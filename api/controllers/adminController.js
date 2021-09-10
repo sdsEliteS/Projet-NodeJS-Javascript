57085dce2f22a56dd5848a9fab6b6094470acbd9
sdsEliteS <dossantos.steven72190@gmail.com>
2021-09-10 10:13:54 +0200

    DELETE Liste Utilisateur

--- api/controllers/adminController.js
+++ api/controllers/adminController.js
@@ -44,6 +44,25 @@ exports.editUser = (req, res) => {
 exports.deleteUser = async (req, res) => {
     console.log('Suppression Utilisateur', req.body, req.params)
 
+    // Requête SQL "DELETE FROM" permettant de supprimer un article de la Page Admin Section Liste d'Utilisateur //
+    await query(`DELETE FROM User WHERE id = ${ req.params.id }`)
+
+    // Permet de rediriger l'Utilisateur vers l'URL /admin HTML Handlebars + adminController - "openMessage: show" permettant lors de la Suppression de rester sur la page Admin Section Liste Message //
+    const dbUsers = await query('select * from User')
+    const dbArticle = await query('select * from Article')
+    const dbMessage = await query('select * from Message')
+
     // Permet de rediriger (redirect) l'Utilisateur vers l'URL /admin HTML Handlebars + adminController  //
-    res.redirect('/admin')
+    res.render('admin', {
+        articles: dbArticle,
+        messages: dbMessage,
+        users: dbUsers,
+        noFooter: true,
+        openUser: 'show'
+    })
+
+    // res.redirect('/admin') redirige vers des URL et afin qu'on nous renvoi la Page Admin Section Liste Utilisateur au moment de la Suppression de l'Utilisateur,
+    // il faut renvoyer un ficher handlebars et donc la res.render permet une issue positif (res.redirect peut être modifier par un res.render) //
+
+    // res.redirect et res.render font partie de la méthode GET //
 }
