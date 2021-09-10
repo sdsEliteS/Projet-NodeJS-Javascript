30f62822ce6a3fa672eb7cf15f84c6f4ae237387
sdsEliteS <dossantos.steven72190@gmail.com>
2021-09-10 16:36:20 +0200

    Rectification

--- api/controllers/presentationController.js
+++ api/controllers/presentationController.js
@@ -7,27 +7,3 @@ exports.getPagePresentation = (req, res) => {
     // Par default intégration layout main => {{{ body }}} - (Page View)
     res.render('presentation');
 }
-
-
-
-
-// Lors du remplissage du formulaire de contact de la page Présentation //
-
-exports.formContact2 = (req, res) => {
-    console.log('Controller Form Contact2', req.body)
-
-    let sql = `insert into Message (nom, email, sujet, message, date) values (?)`;
-    let values = [
-        req.body.nom,
-        req.body.email,
-        req.body.sujet,
-        req.body.message,
-        req.body.date,
-    ];
-    query(sql, [values], function (err, data, fields) {
-        if (err) throw err;
-
-        // Permet de rediriger (redirect) l'Utilisateur vers l'URL / (home) //
-        res.redirect('/')
-    })
-}
