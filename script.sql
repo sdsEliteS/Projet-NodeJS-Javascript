a0716af42b72cc80ced06199153fd6a7d87fbe77
sdsEliteS <dossantos.steven72190@gmail.com>
2021-09-12 20:08:40 +0200

    Entrainement Commit

--- script.sql
+++ script.sql
@@ -1,4 +1,4 @@
-/* SCRIPT DE LA BASE DE DONNE SQL AVEC LES COMMANDES SQL QU'IL FAUDRA ECRIRE DANS LE TERMINAL DE COMMANDE */
+/* Exemple : SCRIPT DE LA BASE DE DONNE SQL AVEC LES COMMANDES SQL QU'IL FAUDRA ECRIRE DANS LE TERMINAL DE COMMANDE */
 
 insert into User (pseudo, email, password, isAdmin, isVerified, isBan, avatar, address, telephone, birthday)
 values("steven", "dossantos.steven72190@gmail.com", "123456", 1, 1, 0, "monimage", "327 route de beauchêne la bellangerie", "06.61.53.87.18", "1989-06-28");
