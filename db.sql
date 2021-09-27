de6a9fa510ba802daaf9c1b9d155981745ab3059
sdsEliteS <dossantos.steven72190@gmail.com>
2021-09-27 12:17:51 +0200

    Entrainement Commit

--- db.sql
+++ db.sql
@@ -109,4 +109,4 @@ SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 
 insert into User (pseudo, email, password, address, telephone, birthday)
-values ('Steven', 'dossantos.steven72190@gmail.com', '123456', '327 Route de Beauchêne La Béllangerie', '0661538718', '1989-06-28')
+values ('Steven', 'dossantos.steven72190@gmail.com', '123456', '327 Route de Beauchêne La Bellangerie', '0661538718', '1989-06-28')
