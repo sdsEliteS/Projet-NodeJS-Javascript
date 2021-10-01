20b14cd0e44e849d3f8f3f801c58d91512512fe5
sdsEliteS <dossantos.steven72190@gmail.com>
2021-10-01 11:12:19 +0200

    Bannière temporaire setTimeout

---
+++ ReAdMe.md
@@ -0,0 +1,6 @@
+Liens Javascript :
+
+
+# setTimeout (Le temps en seconde = Pratique pour les bannières temporaire (Validation ou Error après le remplissage du formulaire register))
+
+https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_win_settimeout
--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -3,8 +3,6 @@
  * ********** */
 
 
-
-
 /**************************************************************************** METHODE ASYNCHRONE **************************************************************************************************************************************************************/
 // Visualisation de la Page LOGIN ( READ/Lire = METHOD GET = MySQL: SELECT ) //
 // Exportation de la routes du router.js (getPageLogin) dans le Controller avec => une Function opérant un retour d'information en rapport avec la methode GET - req = requête HTTP Utilisateur faite au Server et res = response du Server //
@@ -71,7 +69,7 @@ exports.connexionProfil = async (req, res) => {
         noFooter: true,
 
         // Tableau "users" en rapport avec la requête SQL de la table User dans mydb (Base de Donnée - Fichier db.sql) //
-        user 
+        user
     });
 
 
@@ -122,7 +120,6 @@ exports.forgetProfil = (req, res) => {
 // Exportation de la routes du router.js dans le Controller (getPageRegister) avec => une Function opérant un retour d'information en rapport avec la methode GET - req = requête HTTP de Utilisateur faite au Server et res = response du Server //
 exports.getPageRegister = (req, res) => {
 
-
     // Par default intégration layout main => {{{ body }}} - (Page View)
     // Server renvoi à l'Utilisateur un fichier Handlebars HTML 'register' se situant dans le DOSSIER views //
     res.render('register', {
@@ -140,26 +137,51 @@ exports.getPageRegister = (req, res) => {
 /**************************************************************************** METHODE SYNCHRONE **************************************************************************************************************************************************************/
 // Remplissage du formulaire d'enregistrement de l'Utilisateur de la page REGISTER ( CREATE = Method POST HTTP = Requête MySQL: INSERT INTO ) // ID s'auto_increment donc pas besoin de le mentionner dans la Requête SQL //
 // export de la routes du router.js (registerProfil) avec => une Function opérant un retour d'information en rapport avec la methode POST - req = requete HTTP de utilisateur faite au server et res = response du server //
-exports.registerProfil = (req, res) => {
+exports.registerProfil = async (req, res) => {
     console.log('Enregistrement Compte Steven', req.body)
 
-    // Requête SQL permettant la création de plusieurs colonnes dans la Table User //
-    // req.body permet de nous ressortir les données dans un terminal de commande afin de constater du bon fonctionnement de l'applis lors du remplissage du formulaire d'enregistrement du l'Utilisateur visionnant les données au moment de la validation //
-    let sql = `INSERT INTO User (pseudo, email, password, address, telephone, birthday) values (?)`;
-    let values = [
-        req.body.pseudo,
-        req.body.email,
-        req.body.mot_de_passe,
-        req.body.adresse,
-        req.body.telephone,
-        req.body.date_de_naissance
-    ];
+    const userExist = await query(`SELECT * FROM User where pseudo = '${ req.body.pseudo }'`)
 
-    // Synchronous version
-    // Valeur des colonnes de la Table User qui sont écrit dans les input - Method Synchrone  //
-    query(sql, [values], function (err, data, fields) {
-        if (err) throw err;
-        // Server permet de rediriger (redirect) l'Utilisateur vers l'URL / (home) au moment de la validation du formulaire de la page REGISTER //
-        res.redirect('/')
-    })
+
+    console.log('UserExist', userExist)
+
+    if (userExist.length > 0) {
+        if (userExist[0].pseudo.toLowerCase() === req.body.pseudo.toLowerCase() ) {
+            res.render('register', {
+                error: 'Ce pseudo est déja pris !'
+            })
+        }
+        
+    } else if (!userExist[0] && req.body.mot_de_passe.length < 8) {
+        res.render('register', {
+            error: 'Le mot de passe contien moins de 8 charactèrs'
+        })
+
+    } else {
+        console.log('UserNotExist')
+
+        // Requête SQL permettant la création de plusieurs colonnes dans la Table User //
+        // req.body permet de nous ressortir les données dans un terminal de commande afin de constater du bon fonctionnement de l'applis lors du remplissage du formulaire d'enregistrement du l'Utilisateur visionnant les données au moment de la validation //
+        let sql = `INSERT INTO User (pseudo, email, password, address, telephone, birthday) values (?)`;
+        let values = [
+            req.body.pseudo,
+            req.body.email,
+            req.body.mot_de_passe,
+            req.body.adresse,
+            req.body.telephone,
+            req.body.date_de_naissance
+        ];
+
+        // Synchronous version
+        // Valeur des colonnes de la Table User qui sont écrit dans les input - Method Synchrone  //
+        query(sql, [values], function (err, data, fields) {
+            if (err) throw err;
+            // Server permet de rediriger (redirect) l'Utilisateur vers l'URL / (home) au moment de la validation du formulaire de la page REGISTER //
+            res.render('register', {
+                success: 'Votre compte à bien été créé !'
+            })
+        })
+
+    }
+
 }
