f787d9cc07ce59416377f47f038d523216a0e7d1
sdsEliteS <dossantos.steven72190@gmail.com>
2021-11-10 16:32:09 +0100

    Entrainement Commit

--- api/controllers/blogController.js
+++ api/controllers/blogController.js
@@ -146,7 +146,7 @@ exports.editArticle = async (req, res) => {
                 SET title = '${ req.body.title }',
                     description = '${ req.body.description }',
                     recommandation = '${ req.body.recommandation }',
-                    dateEdit = '${ DATEEDIT }',
+                    dateEdit = '${ moment().format('YYYY-MM-DD') }',
                     subdescription = '${ req.body.subdescription }'
                 WHERE id = '${ req.params.id }';`
         // req.params est l'id donner en paramètre de l'URL (/Article/:id exemple: /Article/1) permettant d'édit l'id de l'Article qu'on souhaite (1,2,3 ou 4 etc....) s'il y en plusieurs également - Information sur l'édition de l'id mentionner également dans le terminal de commande (Chaque Article à un numero d'id précis //
@@ -178,7 +178,7 @@ exports.editArticle = async (req, res) => {
                 SET title = '${ req.body.title }',
                     description = '${ req.body.description }',
                     recommandation = '${ req.body.recommandation }',
-                    dateEdit = '${ DATEEDIT }',
+                    dateEdit = '${ moment().format('YYYY-MM-DD') }',
                     image = '${ req.file.nomComplet }',
                     subdescription = '${ req.body.subdescription }'
                 WHERE id = '${ req.params.id }';`
