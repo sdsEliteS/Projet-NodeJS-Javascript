9573c0fccc02459dbafb806a90efb7d0f4622308
sdsEliteS <dossantos.steven72190@gmail.com>
2021-12-06 11:30:42 +0100

    Entrainement Commit

--- db copy.sql
+++ db copy.sql
@@ -28,7 +28,7 @@ CREATE TABLE IF NOT EXISTS `mydb`.`User` (
   `isAdmin` TINYINT NOT NULL DEFAULT 0,
   `isVerified` TINYINT NOT NULL DEFAULT 0,
   `isBan` TINYINT NOT NULL DEFAULT 0,
-  `avatar` VARCHAR(255) NOT NULL DEFAULT './default-profil.png',
+  `avatar` VARCHAR(255) NOT NULL DEFAULT 'default-profile.png',
   `address` VARCHAR(255) NOT NULL,
   `telephone` VARCHAR(255) NOT NULL,
   `birthday` DATE NOT NULL,
