b63a9a6be0696aeac3a14f2ffc60939e81f7db48
sdsEliteS <dossantos.steven72190@gmail.com>
2021-12-23 14:46:54 +0100

    Entrainement Commit

--- api/middleware/admin.js
+++ api/middleware/admin.js
@@ -1,8 +1,8 @@
 // Permet de checker si la session est admin et si OUI, donne accès à la page admin - SÉCURITÉ afin qu'un visiteur ou un utilisateur ne fasse pas un /admin et arriver sur le panneautage ADMIN sans être ADMIN //
 module.exports = (req, res, next) => {
+
     console.log('Je suis le Middleware', req.session.isAdmin)
     if (req.session.isAdmin === true) next()
     else res.redirect('/')
 }
-
 // Un middleware est un tampon bloquant les acccès empêchant l'exécution d'une fonction callback d'un controller //
