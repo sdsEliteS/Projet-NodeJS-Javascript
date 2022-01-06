577b509d490c34c86939eba60c68ef3af295fb79
sdsEliteS <dossantos.steven72190@gmail.com>
2022-01-06 16:55:01 +0100

    Entrainement Commit

--- api/middleware/admin.js
+++ api/middleware/admin.js
@@ -5,4 +5,4 @@ module.exports = (req, res, next) => {
     if (req.session.isAdmin === true) next()
     else res.redirect('/')
 }
-// Un middleware est un tampon bloquant les acccès empêchant l'exécution d'une fonction callback d'un controller //
+// Un middleware est un tampon bloquant les accès empêchant l'exécution d'une fonction callback d'un controller //
